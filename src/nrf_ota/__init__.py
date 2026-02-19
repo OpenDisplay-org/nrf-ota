6dfccbe601820008535b298f10b05798bb2bcd69
gabriel <admin@g4bri3l.de>
2026-02-19 15:16:03 +0100

    chore: initial commit

---
+++ src/nrf_ota/__init__.py
@@ -0,0 +1,17 @@
+"""nrf-ota.
+
+Python package for OTA updating the bootloader on nrf chips
+"""
+
+# TODO: Import your public API here
+# Example:
+# from .core import main_function
+# from .models import MyModel
+# from .enums import MyEnum
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # TODO: List your public API exports here
+    # Export only what users should import directly
+]
